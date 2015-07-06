7eaf6bc2e04e33e9cca56a6435e6c31570100f34
MrLowkos <qdefois@gmail.com>
2015-07-06 22:24:11 +0200

    Add JUnit

---
+++ tests/com/eldotk/core/EntityManagerTest.java
@@ -0,0 +1,19 @@
+package com.eldotk.core;
+
+import org.junit.*;
+
+/**
+ * Created by Elka on 06/07/2015.
+ */
+public class EntityManagerTest {
+
+    @Test
+    public void testCreateEntity() throws Exception {
+
+    }
+
+    @Test
+    public void testDestroyEntity() throws Exception {
+
+    }
+}
